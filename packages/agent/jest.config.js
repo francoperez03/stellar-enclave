f78a37323e751a7a5e45971f845369fa15e1a996
francoperez03 <franco.perez03@gmail.com>
2026-04-12 15:10:09 -0300

    chore(03-01): add Jest + ts-jest infrastructure and gitignore patterns
    - Add jest 29.7.0, ts-jest, @types/jest, @types/node to agent devDeps
    - Add test/test:watch scripts to packages/agent/package.json
    - Create packages/agent/jest.config.js with ESM + ts-jest transform
    - Add *.enclave.json and *-notes.json to .gitignore (SDK-05)

---
+++ packages/agent/jest.config.js
@@ -0,0 +1,19 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+      tsconfig: {
+        module: 'ESNext',
+        moduleResolution: 'bundler',
+      },
+    }],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  roots: ['<rootDir>/src'],
+};
